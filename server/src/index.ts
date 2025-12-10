6c3db343fb0bff0dd81ab595df4d5fb3f1a4e715
Zakiya <zakiyamanzoor04@gmail.com>
2025-12-10 21:57:05 +0530

    add postgress basics

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,64 +1,23 @@
-/*import http, { IncomingMessage, ServerResponse } from "http";
-
-const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
-  // Handle CORS
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  //these are preflight ones
-  if (req.method === "OPTIONS") {
-    res.writeHead(204);
-    res.end();
-    return;
-  }
-
-  const url = req.url ?? "";
-  const method = req.method ?? "";
-
-  if (url === "/api/notes" && method === "GET") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ message: "success!" }));
-  } else if (url === "/api/notes" && method === "POST") {
-    let body = "";
-
-    // Collect the data chunks
-    req.on("data", (chunk: Buffer) => {
-      body += chunk.toString();
-    });
-
-    // When all data has been received
-    req.on("end", () => {
-      try {
-        const parsed = JSON.parse(body);
-        res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ received: parsed }));
-        //youre just sending it back cause you dont have dB
-      } catch {
-        res.writeHead(400, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ error: "Invalid JSON" }));
-      }
-    });
-  } else {
-    res.writeHead(404, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ error: "Not found" }));
-  }
-});
-
-server.listen(5000, () => {
-  console.log("Server running on localhost:5000");
-});
-
-//what all this server is doing is eithre getting the data or posting when it gets it is saying yes i got it but when it is posting then it just sends the data back to the brower?
-*/
-
 
 import http,{IncomingMessage,ServerResponse} from "http";
+import {Pool} from "pg";
 
+const pool:Pool = new Pool({
+    user:'Zakiya',
+    password:'password',
+    host:'localhost',
+    port:5432,
+    database:'noteo'
+})
 const server = http.createServer((req:IncomingMessage,res:ServerResponse)=>{
+
 //what things i want from the browser
+
 res.setHeader("Access-Control-Allow-Origin", "*");
 res.setHeader("Access-Control-Methods", "GET,POST,OPTIONS");
 res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+// at first what will happen
 
 if(req.method == 'OPTIONS'){
     res.writeHead(204);
@@ -66,6 +25,8 @@ if(req.method == 'OPTIONS'){
     return;
 }
 const method = req.method ?? "";
+
+// what will i give to requests 
 
 if(method == 'GET'){
     res.writeHead(200, {"Content-type":"application/json"});
