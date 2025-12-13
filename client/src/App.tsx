61604a1bf79e0aaad3b65adb522c9acec4b4d5f2
Zakiya <zakiyamanzoor04@gmail.com>
2025-12-13 21:47:54 +0530

    minor assembling changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Side from '../components/side';
+import Notes from "../components/notes";
 
 function App() {
 
   return (
   
-  <div className='bg-black h-screen'></div>
+  <div className='bg-black h-screen flex'>
+    <Side/>
+   <Notes/>
+  </div>
   
   )
 }
