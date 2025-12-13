470b69d2aff6488434a0afd49f2fd012733eaf9e
Zakiya <zakiyamanzoor04@gmail.com>
2025-12-13 21:47:29 +0530

    removed the button

--- client/components/side.tsx
+++ client/components/side.tsx
@@ -1,12 +1,10 @@
 
-function handleClick(){
-  
-}
+
 function Side() {
   return (
     <div className="border-r border-gray-400 h-full w-1/8 p-4">
-      <h1 className="text-white text-5xl font-medium ">Noteo</h1>
-      <button className="text-4xl font-normal p-3 m-4 rounded-full w-14 h-14 flex items-center justify-center bg-white" onClick={handleClick}>+</button>
+      <h1 className="text-white text-4xl font-medium ">Noteo</h1>
+      
     </div>
   )
 }
